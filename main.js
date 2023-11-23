ee3625e29395ce6467554f100bc035c4a84204b5
habberzabib <145040111+habberzabib@users.noreply.github.com>
2023-11-22 21:32:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Dice Roll Simulator Demo
+// Dice Roll Simulator
 
 // HTML Element for Output
 let outputEl = document.getElementById("output");
@@ -10,7 +10,7 @@ function goBtnClicked() {
   // Get Menu Selection
   let selection = document.getElementById("menu").value;
 
-  // Process Selection
+  // Function Selection
   if (selection === "roll-once") {
     rollOnce();
   } else if (selection === "roll-five") {
@@ -26,56 +26,67 @@ function rollOnce() {
   // Display Menu Title
   outputEl.innerHTML = "<h3>ROLL ONCE</h3>";
 
-  // Random Dice Roll & Total
+  // Random dice roll and total
   let die1 = Math.floor(Math.random() * 6) + 1;
   let die2 = Math.floor(Math.random() * 6) + 1;
   let total = die1 + die2;
-
+  
   // Add Results to output element
   let pEl = document.createElement("p");
   pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
   outputEl.appendChild(pEl);
+  
   let snakeEyesEl = document.getElementById("snakeEyesEl");
   if (snakeEyesEl) {
-    snakeEyesEl.style.display = "none"; // Hide snakeEyesEl if it exists
+    snakeEyesEl.style.display = "none"; // Hide snakeEyesEl if it exists anywhere but the function snakeEyes
   }
 }
 
 function roll5Times() {
-  // DISPLAY MENU TITLE
+  // Display Menu Title
   outputEl.innerHTML = "<h3>ROLL 5 TIMES</h3>";
 
-  //RANDOM DICE ROLL & TOTAL
+  //set n value to 0 to begin, and as long as n is less than 5, keep rolling 
   for (n = 0; n < 5; n++) {
+    
+     // Random dice roll and total
     let die1 = Math.floor(Math.random() * 6) + 1;
     let die2 = Math.floor(Math.random() * 6) + 1;
     let total = die1 + die2;
+    
+    // Add Results to output element
     let pEl = document.createElement("p");
     pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
     outputEl.appendChild(pEl);
   }
+  
   let snakeEyesEl = document.getElementById("snakeEyesEl");
   if (snakeEyesEl) {
-    snakeEyesEl.style.display = "none"; // Hide snakeEyesEl if it exists
+    snakeEyesEl.style.display = "none"; // Hide snakeEyesEl if it exists anywhere but the function snakeEyes
   }
 }
 
 function rollNTimes() {
-  // DISPLAY MENU TITLE
+  //Display Menu Title
   outputEl.innerHTML = "<h3>ROLL 'N' TIMES</h3>";
 
-  //PROMPT USER TO DEFINE 'n'
+  //Prompt user to define n
   let input = prompt("Input 'n' value");
 
-  //RANDOM DICE ROLL & TOTAL
+  //start with n at 0, and from there, as long as n is less than whatever number the user inputs, keep rolling the dice
   for (let n = 0; n < input; n++) {
+
+    //Random dice and total
     let die1 = Math.floor(Math.random() * 6) + 1;
     let die2 = Math.floor(Math.random() * 6) + 1;
     let total = die1 + die2;
+    
+    // Add Results to output element
     let pEl = document.createElement("p");
     pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
     outputEl.appendChild(pEl);
   }
+  
   let snakeEyesEl = document.getElementById("snakeEyesEl");
   if (snakeEyesEl) {
     snakeEyesEl.style.display = "none"; // Hide snakeEyesEl if it exists
@@ -83,35 +94,39 @@ function rollNTimes() {
 }
 
 function snakeEyes() {
-  // DISPLAY MENU TITLE
+  // Display Menu Title
   outputEl.innerHTML = "<h3>ROLL UNTIL SNAKE EYES</h3>";
 
   let snakeEyesEl = document.getElementById("snakeEyesEl");
-  snakeEyesEl.style.display = "block"; // Show the text
+  snakeEyesEl.style.display = "block"; // Show snakeEyesEl text
 
-  let snakeEyesCountEl = document.getElementById("snakeEyesCount");
-  snakeEyesCountEl.textContent = "0"; // Reset the count
+  // Declare the counter
+  let snakeEyesCount = 0;
 
-  let numSnakeEyes = 0;
-
-  // Define total, die1, and die2 OUTSIDE the loop but do not declare them
+  // Declare total, die1, and die2 OUTSIDE the loop
   let total, die1, die2;
 
-  // do...while statement: loop code while both dice do NOT = 1
+  // do...while statement: roll at least once; loop code while both dice do NOT = 1
   do {
+    //Random dice and total
     die1 = Math.floor(Math.random() * 6) + 1;
     die2 = Math.floor(Math.random() * 6) + 1;
     total = die1 + die2;
 
+    // Add Results to output element
     let pEl = document.createElement("p");
     pEl.innerHTML = `${die1},${die2} (sum: ${total})`;
     outputEl.appendChild(pEl);
 
-    // numSnakeEyes only when both dice are NOT 1
+    // add one to snakeEyesCount only when both dice do NOT = 1
     if (die1 !== 1 && die2 !== 1) {
-      numSnakeEyes++;
-      snakeEyesCountEl.textContent = numSnakeEyes;
+      snakeEyesCount++;
     }
-    // repeat until both dice = 1
+    // Repeat until both dice = 1 
   } while (die1 !== 1 || die2 !== 1);
+
+  // Set the final count after the loop finishes
+  let snakeEyesCountEl = document.getElementById("snakeEyesCount");
+  snakeEyesCountEl.innerHTML = snakeEyesCount;
 }
+
